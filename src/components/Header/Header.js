4672b9dd045ede5d6f079702010a4c9235bbc218
vaishnavme <vaishnavchandurkar21@outlook.com>
2021-02-03 13:05:20 +0530

    Feat and Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,16 @@ const Header = () => {
     return (
         <div className="header">
             <div className="hero-description">
-                <h1>Welcome</h1>
-                <p> If you are building a hybrid app (some UIs in React Native, some UIs in platform code) you can still use images that are already bundled into the app. </p>
+                <h1>Welcome to NameeO</h1>
+                <p className="text">Get creative name ideas as for your new Project or Startup.</p>
+                <ul className="features">
+                    <li>
+                        <p><b>🎴 Availability: </b> Check if name is avaiable to Register on new Domain.</p>
+                    </li>
+                    <li>
+                        <p><b>🌿 Uniqueness: </b> Get list of all names accourding to your Keyword.</p>
+                    </li>
+                </ul>
             </div>
             <div className="hero-illustration">
                 <img src={process.env.PUBLIC_URL + '/images/Hero.svg'} className="hero-img"></img>
