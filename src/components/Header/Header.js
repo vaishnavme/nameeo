4a3e4f91f3bca52ad794c962b4ddd9ae3d45178c
vaishnavme <vaishnavchandurkar21@outlook.com>
2021-02-03 12:18:59 +0530

    Feat:  basic header layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <h1>Head</h1>
+        <div className="header">
+            <div className="hero-description">
+                <h1>Welcome</h1>
+                <p> If you are building a hybrid app (some UIs in React Native, some UIs in platform code) you can still use images that are already bundled into the app. </p>
+            </div>
+            <div className="hero-illustration">
+                <img src={process.env.PUBLIC_URL + '/images/Hero.svg'} className="hero-img"></img>
+            </div>
+        </div>
     )
 }
 
