91d7ca5478be00747ed8979b0cd43e8cf0fe4dee
vaishnavme <vaishnavchandurkar21@outlook.com>
2021-02-03 16:04:42 +0530

    Restyle Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,13 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
+            
+            <div className="hero-illustration">
+                <img src={process.env.PUBLIC_URL + '/images/Hero.svg'} className="hero-img"></img>
+            </div>
             <div className="hero-description">
                 <h1>Welcome to NameeO</h1>
-                <p className="text">Get creative name ideas as for your new Project or Startup.</p>
+                <p className="text">Get creative name ideas for your new Project or Startup.</p>
                 <ul className="features">
                     <li>
                         <p><b>🎴 Availability: </b> Check if name is avaiable to Register on new Domain.</p>
@@ -15,9 +19,6 @@ const Header = () => {
                         <p><b>🌿 Uniqueness: </b> Get list of all names accourding to your Keyword.</p>
                     </li>
                 </ul>
-            </div>
-            <div className="hero-illustration">
-                <img src={process.env.PUBLIC_URL + '/images/Hero.svg'} className="hero-img"></img>
             </div>
         </div>
     )
